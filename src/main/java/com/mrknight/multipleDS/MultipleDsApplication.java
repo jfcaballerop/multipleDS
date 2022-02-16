fa67758edbe9f58da6ada12e75f57a236fa7fba4
Jose Fernando Caballero <josefernando.caballero@logesta.com>
2022-02-16 16:07:45 +0100

    initial DS

---
+++ src/main/java/com/mrknight/multipleDS/MultipleDsApplication.java
@@ -0,0 +1,13 @@
+package com.mrknight.multipleDS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleDsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleDsApplication.class, args);
+	}
+
+}
